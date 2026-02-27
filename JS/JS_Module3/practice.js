c8f40ae907e414a620bfa288cd9530edd3258d23
Justin Kenneth Mamaril <qjkmamaril01@tip.edu.ph>
2026-02-27 14:58:32 +0800

    Done Module 3

--- JS/JS_Module3/practice.js
+++ JS/JS_Module3/practice.js
@@ -1,3 +1,14 @@
+console.log(2 + 3 + 2);      // 7  
+console.log(2 ** 4);         // 16  
+console.log(5 / 1);          // 5  
+console.log(8 * 2 + 5 - 2);  // 39
+
+console.log(4 * 5 === 20);     
+console.log(6 * 5 == "30");    
+console.log(-17 < 0);          
+console.log(25 > 1);           
+console.log(2 + 2 * 2 !== 4);  
+
 console.log(true || false);  
 console.log(false == false);  
 console.log(false == false || true);  
